57ae8644612908a242cddb9de11bb5cf310f4e98
jinkieu <quachjulien@gmail.com>
2020-04-17 21:01:43 +0200

    modif login

--- src/App.js
+++ src/App.js
@@ -2,28 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
-import SignInForm from './SignInForm';
-import SignUpForm from './SignUpForm';
+import { withAuthenticator } from "aws-amplify-react";
+
 Amplify.configure(aws_exports);
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      signedUp : false
-    }
-    this.handleSignup = this.handleSignUp.bind(this);
-  }
 
-  handleSignUp() {
-    this.setState({
-      signedUp: true
-    });
-  }
   render() {
-    const { signedUp } = this.state;
-    return !signedUp ? <SignUpForm handleSignup={ this.handleSignUp }/> : <SignInForm />;
+
+    return(
+
+        <div>
+          <h1>Test</h1>
+        </div>
+
+    );
   }
 }
 
-export default App;
+export default withAuthenticator(App, true);
