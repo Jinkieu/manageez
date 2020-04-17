d8d177789515f53b0264a1c3d8bc0632b631ef92
Nicolas Thong <nicolas.thong@hotmail.com>
2020-04-18 01:40:57 +0200

    Get API

--- src/App.js
+++ src/App.js
@@ -1,86 +1,58 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Products from './components/Products';
-import ProductAdmin from './components/ProductAdmin';
-import LogIn from './components/auth/LogIn';
-import Register from './components/auth/Register';
-import ForgotPassword from './components/auth/ForgotPassword';
-import ForgotPasswordVerification from './components/auth/ForgotPasswordVerification';
-import ChangePassword from './components/auth/ChangePassword';
-import ChangePasswordConfirm from './components/auth/ChangePasswordConfirm';
-import Welcome from './components/auth/Welcome';
-import Footer from './components/Footer';
-import { Auth } from 'aws-amplify';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faEdit } from '@fortawesome/free-solid-svg-icons';
-library.add(faEdit);
+import Amplify from 'aws-amplify';
+import axios from 'axios';
+import aws_exports from './aws-exports';
+
+/*import { withAuthenticator } from "aws-amplify-react";*/
+Amplify.configure(aws_exports);
+axios.defaults.withCredentials = false;
 
 class App extends Component {
-
-  state = {
-    isAuthenticated: false,
-    isAuthenticating: true,
-    user: null
-  }
-
-  setAuthStatus = authenticated => {
-    this.setState({ isAuthenticated: authenticated });
-  }
-
-  setUser = user => {
-    this.setState({ user: user });
-  }
-
-  async componentDidMount() {
-    try {
-      const session = await Auth.currentSession();
-      this.setAuthStatus(true);
-      console.log(session);
-      const user = await Auth.currentAuthenticatedUser();
-      this.setUser(user);
-    } catch(error) {
-      if (error !== 'No current user') {
-        console.log(error);
-      }
+    constructor(props) {
+        super(props);
+        this.state = {
+            employees: []
+        }
     }
 
-    this.setState({ isAuthenticating: false });
-  }
-
-  render() {
-    const authProps = {
-      isAuthenticated: this.state.isAuthenticated,
-      user: this.state.user,
-      setAuthStatus: this.setAuthStatus,
-      setUser: this.setUser
+    componentDidMount() {
+        axios.get(`https://bjz5jcmglg.execute-api.eu-west-1.amazonaws.com/Manageez/employe`)
+            .then(res => {
+                const persons = res.data;
+                const employees = persons.Employees;
+                this.setState({ employees });
+                console.log(employees);
+            })
     }
-    return (
-        !this.state.isAuthenticating &&
-        <div className="App">
-          <Router>
+
+    render() {
+        /*if (this.props.authState === "signedIn") {
+            return (
+                <div>
+                    <h1>Internal App</h1>
+                </div>
+            );
+        } else {
+            return null;
+        }*/
+
+        return (
             <div>
-              <Navbar auth={authProps} />
-              <Switch>
-                <Route exact path="/" render={(props) => <Home {...props} auth={authProps} />} />
-                <Route exact path="/products" render={(props) => <Products {...props} auth={authProps} />} />
-                <Route exact path="/admin" render={(props) => <ProductAdmin {...props} auth={authProps} />} />
-                <Route exact path="/login" render={(props) => <LogIn {...props} auth={authProps} />} />
-                <Route exact path="/register" render={(props) => <Register {...props} auth={authProps} />} />
-                <Route exact path="/forgotpassword" render={(props) => <ForgotPassword {...props} auth={authProps} />} />
-                <Route exact path="/forgotpasswordverification" render={(props) => <ForgotPasswordVerification {...props} auth={authProps} />} />
-                <Route exact path="/changepassword" render={(props) => <ChangePassword {...props} auth={authProps} />} />
-                <Route exact path="/changepasswordconfirmation" render={(props) => <ChangePasswordConfirm {...props} auth={authProps} />} />
-                <Route exact path="/welcome" render={(props) => <Welcome {...props} auth={authProps} />} />
-              </Switch>
-              <Footer />
+                {this.state.employees.map((item, index) => {
+                    return (
+                        <div key={index}>
+                            <li>ID : {item.ID_Employe}</li>
+                            <li>Anciennete : {item.Anciennete}</li>
+                            <li>Employe_name : {item.Employe_name}</li>
+                            <li>Emploi : {item.Emploi}</li>
+                            <li>Employe_surname : {item.Employe_surname}</li>
+                        </div>)
+                })}
             </div>
-          </Router>
-        </div>
-    );
-  }
+        )
+
+    }
 }
 
 export default App;
